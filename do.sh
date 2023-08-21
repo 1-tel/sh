7d26909748a5e8241ff77615119f75131d63c69b
1-tel <141106084+1-tel@users.noreply.github.com>
2023-08-22 00:41:49 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,5 +1,8 @@
 #!/bin/bash  
-apt-get remove docker docker-engine docker.io containerd runc
+read -r -p "确认是Debioan吗? [Y/n] " input
+case $input in
+    [yY][eE][sS]|[yY])
+		apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
 apt-get install ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
@@ -11,3 +14,23 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+		;;
+    [nN][oO]|[nN])
+yum remove docker docker-engine docker.io containerd runc
+yum update
+yum install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+yum update
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+       	;;
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
